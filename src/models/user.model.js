554925ce421c0f17486e8f355a013af889118984
LimaGianFranco <gian9710@gmail.com>
2025-07-06 12:05:31 -0300

    corrección

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  first_name: String,
-  last_name: String,
-  email: { type: String, unique: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
   age: Number,
-  password: String,
+  password: { type: String, required: true },
   cart: { type: mongoose.Schema.Types.ObjectId, ref: 'Cart' },
   role: { type: String, default: 'user' }
 });
 
 export const UserModel = mongoose.model('User', userSchema);
-
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -5,22 +5,25 @@ import { validateFields } from '../middlewares/validateFields.js';
 import { authToken, authorizeRoles } from '../middlewares/auth.middleware.js';
 
 const router = Router();
+
 router.post(
   '/register',
   [
+    body('first_name').notEmpty().withMessage('El nombre es obligatorio'),
+    body('last_name').notEmpty().withMessage('El apellido es obligatorio'),
     body('email').isEmail().withMessage('Email inválido'),
-    body('password')
-      .isLength({ min: 6 })
-      .withMessage('La contraseña debe tener al menos 6 caracteres'),
-    validateFields, 
+    body('password').isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres'),
+    validateFields
   ],
   registerUser
 );
 
 router.post('/login', loginUser);
+
 router.get('/current', authToken, (req, res) => {
-  res.json({ user: req.user });  
+  res.json({ user: req.user });
 });
+
 router.get('/admin-dashboard', authToken, authorizeRoles('admin'), (req, res) => {
   res.send('Dashboard de administrador');
 });
